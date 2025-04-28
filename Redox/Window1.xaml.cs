908347259d35efe3579f8dba5134726966c3e4bf
artur <artur@LAPTOP-95DEKHMP>
2025-04-28 01:36:58 -0600

    Agregar archivos de proyecto.

---
+++ Redox/Window1.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Redox
+{
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            MenuS();
+        }
+
+        public virtual bool Multiline { get; set; }
+
+        private SoundPlayer Player = new SoundPlayer();
+        private void MenuS()
+        {
+            this.Player.SoundLocation = "C:\\Users\\artur\\source\\repos\\Redox\\Redox\\Assets\\menu loop.wav";
+                Player.PlayLooping();
+        }
+        private void IniciarJuego(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            this.Close();
+            mainWindow.Show();
+        }
+
+        private void Opciones_Click(object sender, RoutedEventArgs e)
+        {
+            Help_Menu.Visibility = Visibility.Visible;
+        }
+
+        private void Salir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        //Visibility="Hidden"
+
+        private void BackMenu_Click(object sender, RoutedEventArgs e)
+        {
+            Help_Menu.Visibility = Visibility.Hidden;
+        }
+    }
+}
